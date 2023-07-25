af37adc84cae9d64700676413d9dcb255326db8f
Partridge <anhduc130703@gmail.com>
2023-07-25 21:05:42 +0700

    update to react 18

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+        corejs: 2,
+        targets: { node: '6' },
+      },
+    ],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+};
---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import resolve from 'rollup-plugin-node-resolve';
+import filesize from 'rollup-plugin-filesize';
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
+import commonjs from 'rollup-plugin-commonjs';
+import cleanup from 'rollup-plugin-cleanup';
+import json from 'rollup-plugin-json';
+import typescript from 'rollup-plugin-typescript2';
+
+const input = 'src/index.ts';
+
+const plugins = [
+  resolve(),
+  typescript({
+    typescript: require('typescript'),
+  }),
+  commonjs(),
+  json(),
+  cleanup(),
+  filesize(),
+];
+
+const externals = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
+
+export default [
+  {
+    input,
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+    external: externals,
+    plugins,
+  },
+];
